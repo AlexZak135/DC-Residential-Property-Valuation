7f90c282328d4d36bbea54aacf4f3279f8f38668
AlexZak135 <azakrzeski13@gmail.com>
2025-11-06 20:23:49 -0500

    Update

---
+++ Code/DC-Residential-Property-Valuation-Code.py
@@ -0,0 +1,10 @@
+# Title: DC Residential Property Valuation Analysis
+# Author: Alexander Zakrzeski
+# Date: November 6, 2025
+
+# Part 1: Setup and Configuration
+
+# Load to import, clean, and wrangle data
+import polars as pl
+
+# Part 2: Data Preprocessing
